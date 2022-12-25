b9f56259dc01106ffb3da66375a5144c17aeffa3
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2022-12-25 15:24:34 +0100

    on search exercises

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,20 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
-
+import HeroBannerImage from '../assets/images/banner.png'
 const HeroBanner = () => {
   return (
-    <Box sx={{mt:{lg:'212px',xs:'70px'},ml:{sm:'50px'}}} position='relative' p='20px'>
-        <Typography color='#ff2625' fontWeight='600' fontSize='26px'>Fitness Club</Typography>
+    <Box sx={{mt:{lg:'100px',xs:'70px'},ml:{sm:'50px'}}} position='relative' p='20px'>
+        <Typography color='#ff2625' fontWeight='600' fontSize='32px'>Fitness Club</Typography>
+        <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'40px'}}}mb='23px' mt='10px'>
+          Sweat, Smile <br/> and Repeat
+        </Typography>
+        <Typography fontSoze='22px' lineHeight='35px' mb={3}>
+         Check out the most effective exercises
+        </Typography>
+        <Button variant='contained' color='error' href='#exercises' sx={{backgroundColor:'#ff2625',padding:'10px'}}>Explore Exercises</Button>
+        <Typography fontWeight={600} color='#ff2625' fontSize='200px' sx={{opacity:0.1,display:{lg:'block',xs:'none'}}}>Exercise</Typography>
+        <div className='hero-banner-img'>
+        <img src={HeroBannerImage} alt='banner'/></div>
     </Box>
   )
 }
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,16 @@
+import { Box, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems='ceneter' mt='37px' p='20px' justifyContent='center'>
+      <Typography fontWeight={700} mb='50px' textAlign='center' sx={{ fontSize: { lg: '44px', xs: '30px' } }}>
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }} height='76px' value='' onChange={(e) => { }} placeholder='Search Exercises' type='text' />
+      </Box>
+    </Stack>
   )
 }
 
