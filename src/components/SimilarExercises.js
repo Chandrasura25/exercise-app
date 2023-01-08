30380ee052b433d635af29d424b10e16a014799f
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-01-08 08:08:17 +0100

    done

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import {Box,Stack,Typography} from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import HorizontalScrollbar from './HorizontalScrollbar';
 import Loader from './Loader'
 
-const SimilarExercises = ({targetMuscleExercises,equipmentExercises}) => {
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <Box sx={{mt:{lg:'100px',xs:0}}}>
-        <Typography varaint='h3' mb={5}>Exercises that target the same muscle group</Typography>
-        <Stack direction='row' sx={{p:'2',position:'relative'}}>
-            {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader/> }
-        </Stack>
+    <Box sx={{ mt: { lg: '100px', xs: 0 } }}>
+      <Typography variant='h3' mb={5} >Same <span style={{color:'#ff2625', textTransform:'capitalize'}}>muscle group</span> Exercises</Typography>
+      <Stack sx={{ p: '2', position: 'relative' }}>
+        {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
+      </Stack>
+      <Typography mt={4} variant='h3' mb={5}>Exercises with <span style={{color:'#ff2625', textTransform:'capitalize'}}>same equipment</span></Typography>
+      <Stack sx={{ p: '2', position: 'relative' }}>
+        {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
+      </Stack>
     </Box>
   )
 }
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       // 'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-      'X-RapidAPI-Key': '14b9ca4446msh7314e0b0642243cp1269b9jsn8e028a638528',
+      'X-RapidAPI-Key': 'dc96f49ea0mshfbfc8a0fc1da928p14e813jsnbca742817be3',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
